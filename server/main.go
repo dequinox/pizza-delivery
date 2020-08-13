72b36f59d3e5f22cc013888461ecb084c9ac7094
dequinox <umarov.doro@gmail.com>
2020-08-13 17:27:42 +0300

    Add Pizza Service

--- server/main.go
+++ server/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Pizza struct {
 	ID          string `json:"id"`
 	Title       string `json:"title"`
@@ -7,8 +15,30 @@ type Pizza struct {
 	ImageUrl    string `json:"imageUrl"`
 }
 
-var Pizzas []Pizza
+var pizzas []Pizza
+
+func getPizzas(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(pizzas)
+}
+
+func getPizza(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range pizzas {
+		for item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
 
 func main() {
 
+	router := mux.NewRouter()
+
+	router.HandleFunc("/pizzas", getPizzas).Methods("GET")
+	router.HandleFunc("/pizzas/{id}", getPizza).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
