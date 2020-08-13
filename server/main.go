7109c7235b426a16b4fe30f2614b78520c01f3b6
dequinox <umarov.doro@gmail.com>
2020-08-13 16:56:18 +0300

    Add Pizza model

--- server/main.go
+++ server/main.go
@@ -1,4 +1,14 @@
 package main
 
+type Pizza struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	ImageUrl    string `json:"imageUrl"`
+}
+
+var Pizzas []Pizza
+
 func main() {
+
 }
